9a8b1d22a7ff1a9a2aa69131e7c4d011e34db9f3
nohyunha95 <nohyunha95@gmail.com>
2022-11-13 16:46:12 +0900

    init commit

---
+++ src/main/java/com/jenkins/docker/slack/SpringbootJenkinsDockerSlackApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.docker.slack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerSlackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkinsDockerSlackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/docker/slack/SpringbootJenkinsDockerSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.docker.slack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerSlackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
